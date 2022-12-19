a1c4f5fb79f18390d34168f9a70d4637e538b36f
Cássio Pereira <cassiorodp@gmail.com>
2022-12-19 16:17:33 -0300

    fix: change theme

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,7 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
 import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
@@ -42,7 +42,7 @@ function PostContent(props) {
       const { language, value } = code;
       return (
         <SyntaxHighlighter
-          style={atomDark}
+          style={dracula}
           language={language}
           children={value}
         />
